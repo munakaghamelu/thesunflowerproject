1e9760cd76d64405c065dea4d50f0ec1ed10a533
muna-phare <muna@phare.health>
2025-04-10 18:58:52 +0100

    add image, text and rounded buttons to section 2

--- thesunfl0werproject/src/App.js
+++ thesunfl0werproject/src/App.js
@@ -43,8 +43,34 @@ function App() {
       </section>
 
       <section ref={page2Ref} id="page2" className="section page2">
-        <h1>Page 2</h1>
-        <p>This is the second section.</p>
+        <div className="page2-content">
+          <div className="page2-image">
+            <img src="/muna.png" alt="Profile" />
+          </div>
+          <div className="page2-text">
+            {/* <h1>About The Project</h1> */}
+            <p>The Sunflower Project is a spirit-led 
+              campaign by British-born Nigerian artist 
+              BY MUNA KO (by-muhna-ko), documenting her 
+              journey toward her first solo exhibition. 
+              Rooted in faith and healing, the project 
+              will culminate in a charitable showcase 
+              where all artworks will be raffled, raising 
+              funds for Black Minds Matter and The Sutton Trust. 
+              These organisations reflect key parts of Muna’s 
+              story—from her early dreams of educational 
+              access to the importance of mental health 
+              in the Black community. 
+            <br></br>Through storytelling and behind-the-scenes 
+            content, the campaign highlights both her 
+            creative process and those who are planting 
+            seeds of hope in their own lives. At its heart, 
+            The Sunflower Project invites you to reflect, 
+            connect, and experience art as a form of giving.
+            </p>
+            <a href="https://www.instagram.com/thesunfl0werproject/" className="cta-button" target="_blank" rel="noopener noreferrer">FOLLOW THE JOURNEY</a>
+          </div>
+        </div>
       </section>
     </div>
   );
